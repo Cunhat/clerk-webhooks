3175b06b455a1da5d4bb434fae4bc1fba0bdeed3
Tiago Cunha <tiago92cunha@gmail.com>
2023-07-13 23:09:57 +0100

    Add logout button to test webhook

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import Image from "next/image";
+import { SignOutButton } from "@clerk/nextjs";
+import { SignedIn } from "@clerk/clerk-react";
 
 export default function Home() {
   const callApi = async () => {
@@ -9,8 +10,20 @@ export default function Home() {
   };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button onClick={() => callApi()}>Request</button>
+    <main className="flex min-h-screen flex-col items-center p-24">
+      <SignedIn>
+        <SignOutButton>
+          <button className="p-3 inline-flex bg-white rounded-md text-black">
+            Logout
+          </button>
+        </SignOutButton>
+        <button
+          className="p-3 inline-flex bg-white rounded-md text-black"
+          onClick={() => callApi()}
+        >
+          Request
+        </button>
+      </SignedIn>
     </main>
   );
 }
